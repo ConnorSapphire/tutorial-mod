c84cd6cced37c53ef2bc43ecc6bff9bb81284483
Connor Smith <71867196+ConnorSapphire@users.noreply.github.com>
2025-05-27 18:48:59 +1000

    Added Bowl of Sewage with food, poison and nausea effects

--- src/main/java/com/example/ModItems.java
+++ src/main/java/com/example/ModItems.java
@@ -3,8 +3,14 @@ package com.example;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.fabricmc.fabric.api.registry.CompostingChanceRegistry;
 import net.fabricmc.fabric.api.registry.FuelRegistryEvents;
+import net.minecraft.component.type.ConsumableComponent;
+import net.minecraft.component.type.ConsumableComponents;
+import net.minecraft.component.type.FoodComponent;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
+import net.minecraft.item.consume.ApplyEffectsConsumeEffect;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
@@ -21,6 +27,8 @@ public class ModItems {
                 .register((itemGroup) -> itemGroup.add(ModItems.SUSPICIOUS_SUBSTANCE));
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.TOOLS)
                 .register((itemGroup) -> itemGroup.add(ModItems.SUSPICIOUS_SUBSTANCE));
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK)
+                .register((itemGroup) -> itemGroup.add(ModItems.BOWL_OF_SEWAGE));
 
         // Add the suspicious substance to the registry of fuels, with a burn time of 30 seconds.
         // Remember, Minecraft deals with logical based-time using ticks.
@@ -29,8 +37,9 @@ public class ModItems {
             builder.add(ModItems.SUSPICIOUS_SUBSTANCE, 30 * 20);
         });
 
-        // Add the suspicious substance to the composting registry with a 30% chance of increasing the composter's level.
-        CompostingChanceRegistry.INSTANCE.add(ModItems.SUSPICIOUS_SUBSTANCE, 0.9f);
+        // Add the suspicious substance to the composting registry with a 70% chance of increasing the composter's level.
+        CompostingChanceRegistry.INSTANCE.add(ModItems.SUSPICIOUS_SUBSTANCE, 0.7f);
+        CompostingChanceRegistry.INSTANCE.add(ModItems.SUSPICIOUS_SUBSTANCE, 1f);
     }
 
     public static Item register(String name, Function<Item.Settings, Item> itemFactory, Item.Settings settings) {
@@ -47,4 +56,15 @@ public class ModItems {
     }
 
     public static final Item SUSPICIOUS_SUBSTANCE = register("suspicious_substance", SuspiciousSubstance::new, new Item.Settings());
+
+    public static final ConsumableComponent BOWL_OF_SEWAGE_CONSUMABLE_COMPONENT = ConsumableComponents.food()
+            .consumeEffect(new ApplyEffectsConsumeEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 6 * 20, 1), 1.0f))
+            .consumeEffect(new ApplyEffectsConsumeEffect(new StatusEffectInstance(StatusEffects.POISON, 6 * 20, 1), 1.0f))
+            .build();
+    public static final FoodComponent BOWL_OF_SEWAGE_FOOD_COMPONENT = new FoodComponent.Builder()
+            .nutrition(5)
+            .saturationModifier(5)
+            .alwaysEdible()
+            .build();
+    public static final Item BOWL_OF_SEWAGE = register("bowl_of_sewage", Item::new, new Item.Settings().food(BOWL_OF_SEWAGE_FOOD_COMPONENT, BOWL_OF_SEWAGE_CONSUMABLE_COMPONENT));
 }
